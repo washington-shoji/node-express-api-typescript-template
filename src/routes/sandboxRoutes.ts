518bca57deafc6b3607c08df48c1108d1c6f66eb
washington-shoji <shoji.washington@gmail.com>
2022-01-12 18:45:57 +1100

    added sandbox CRUD routers to the app

--- src/routes/sandboxRoutes.ts
+++ src/routes/sandboxRoutes.ts
@@ -2,12 +2,22 @@ import userController from '../features/sandbox/controller/sandboxController.con
 import express from 'express';
 export const sandboxRouter = express.Router();
 
-const baseApi = '/sandbox/create';
+const baseApi = '/sandbox';
 
 sandboxRouter.post(`${baseApi}`, (req, res, next) => {
     userController.create(req, res, next);
 });
 
+sandboxRouter.put(`${baseApi}/:id`, (req, res, next) => {
+    userController.updateSandboxById(req, res, next);
+});
+sandboxRouter.delete(`${baseApi}/:id`, (req, res, next) => {
+    userController.deleteSandboxById(req, res, next);
+});
+
+sandboxRouter.get(`${baseApi}/:id`, (req, res, next) => {
+    userController.getSandboxById(req, res, next);
+});
 sandboxRouter.get(`${baseApi}`, (req, res, next) => {
-    res.status(200).json('API Working');
+    userController.getSandbox(req, res, next);
 });
