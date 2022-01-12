1e1eb4f87f8f21bc95c2271b51493a3d4f80e79d
washington-shoji <shoji.washington@gmail.com>
2022-01-12 18:44:40 +1100

    added CRUD methods to the sandbox services

--- src/features/sandbox/service/sandbox.service.ts
+++ src/features/sandbox/service/sandbox.service.ts
@@ -1,6 +1,6 @@
+import { Request } from 'express';
 import { ISandboxDocument } from '../interface/sandbox.interface';
 import sandboxModel from '../model/sandbox.model';
-//import { ISandbox } from './../interface/sandbox.interface';
 
 class SandboxService {
     private sandbox = sandboxModel;
@@ -20,6 +20,80 @@ class SandboxService {
             return result;
         } catch (error) {
             throw new Error('Unable to create a sandbox entry');
+        }
+    }
+
+    /**
+     * Update a sandbox entry by id
+     */
+    public async updateSandboxEntry(req: Request): Promise<ISandboxDocument> {
+        try {
+            const id = req.params.id;
+            const { title, body } = req.body;
+
+            const result = await this.sandbox.findByIdAndUpdate(
+                id,
+                { title, body },
+                { returnDocument: 'after' }
+            );
+            if (!result) {
+                throw new Error(`Unable to find sandbox with id: ${id}`);
+            }
+            return result;
+        } catch (error) {
+            throw new Error('Unable to update a sandbox entry');
+        }
+    }
+
+    /**
+     * Get all sandbox entries
+     */
+
+    public async getSandboxEntries(): Promise<ISandboxDocument[]> {
+        try {
+            const result = await this.sandbox.find({});
+            if (!result) {
+                throw new Error(`Unable to fetch sandbox entries`);
+            }
+            return result;
+        } catch (error) {
+            throw new Error('Unable to get sandbox entries');
+        }
+    }
+
+    /**
+     * Get a sandbox entry by id
+     */
+
+    public async getSandboxEntriesById(
+        req: Request
+    ): Promise<ISandboxDocument> {
+        try {
+            const id = req.params.id;
+            const result = await this.sandbox.findById(id);
+            if (!result) {
+                throw new Error(`Unable to fetch sandbox entry ${id}`);
+            }
+            return result;
+        } catch (error) {
+            throw new Error('Unable to get sandbox entry');
+        }
+    }
+
+    /**
+     * Delete a sandbox entry by id
+     */
+    public async deleteSandboxEntry(req: Request): Promise<ISandboxDocument> {
+        try {
+            const id = req.params.id;
+
+            const result = await this.sandbox.findByIdAndDelete(id);
+            if (!result) {
+                throw new Error(`Unable to find sandbox with id: ${id}`);
+            }
+            return result;
+        } catch (error) {
+            throw new Error('Unable to delete a sandbox entry');
         }
     }
 }
