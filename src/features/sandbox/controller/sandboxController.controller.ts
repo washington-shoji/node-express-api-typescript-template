3765a876e8c3c95219447e43d7b9e50c9f301ecf
washington-shoji <shoji.washington@gmail.com>
2022-01-12 18:43:19 +1100

    added sandbox CRUD controllers to the app

--- src/features/sandbox/controller/sandboxController.controller.ts
+++ src/features/sandbox/controller/sandboxController.controller.ts
@@ -59,6 +59,74 @@ export class UserController {
             next(new HttpException(500, error.message));
         }
     }
+
+    public async updateSandboxById(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            const result = await this.sandboxService.updateSandboxEntry(req);
+
+            res.status(200).json({
+                message: 'Sandbox entry successfully updated',
+                result,
+            });
+        } catch (error: any) {
+            next(new HttpException(500, error.message));
+        }
+    }
+
+    public async deleteSandboxById(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            const result = await this.sandboxService.deleteSandboxEntry(req);
+
+            res.status(200).json({
+                message: 'Sandbox entry successfully deleted',
+                result,
+            });
+        } catch (error: any) {
+            next(new HttpException(500, error.message));
+        }
+    }
+
+    public async getSandbox(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            const result = await this.sandboxService.getSandboxEntries();
+
+            res.status(200).json({
+                message: 'Sandbox entries successfully fetched',
+                result,
+            });
+        } catch (error: any) {
+            next(new HttpException(500, error.message));
+        }
+    }
+
+    public async getSandboxById(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            const result = await this.sandboxService.getSandboxEntriesById(req);
+
+            res.status(200).json({
+                message: 'Sandbox entries successfully fetched',
+                result,
+            });
+        } catch (error: any) {
+            next(new HttpException(500, error.message));
+        }
+    }
 }
 
 const userController = new UserController(sandboxService);
