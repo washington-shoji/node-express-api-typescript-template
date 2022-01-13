e71c03942c40f9aa4fe584e38b370c8ca95c7748
washington-shoji <shoji.washington@gmail.com>
2022-01-13 17:08:17 +1100

    minor refactor to the sandbox router

--- src/routes/sandboxRoutes.ts
+++ src/routes/sandboxRoutes.ts
@@ -1,6 +1,10 @@
-import sandboxController, {
-    sandboxController2,
-} from '../features/sandbox/controller/sandboxController.controller';
+import {
+    sandboxCreateController,
+    sandboxDeleteController,
+    sandboxUpdateController,
+    sandboxGetByIdController,
+    sandboxGetAllController,
+} from '../features/sandbox/controller/sandboxImpControllers';
 import express from 'express';
 
 export const sandboxRouter = express.Router();
@@ -8,20 +12,20 @@ export const sandboxRouter = express.Router();
 const baseApi = '/sandbox';
 
 sandboxRouter.get(`${baseApi}`, (req, res, next) => {
-    sandboxController.getSandbox(req, res, next);
-});
-sandboxRouter.get(`${baseApi}/:id`, (req, res, next) => {
-    sandboxController.getSandboxById(req, res, next);
+    sandboxGetAllController.handleController(req, res, next);
 });
 
 sandboxRouter.post(`${baseApi}`, (req, res, next) => {
-    sandboxController2.handleController(req, res, next);
-    //sandboxController.create(req, res, next);
+    sandboxCreateController.handleController(req, res, next);
 });
 
-sandboxRouter.put(`${baseApi}/:id`, (req, res, next) => {
-    sandboxController.updateSandboxById(req, res, next);
-});
 sandboxRouter.delete(`${baseApi}/:id`, (req, res, next) => {
-    sandboxController.deleteSandboxById(req, res, next);
+    sandboxDeleteController.handleController(req, res, next);
+});
+sandboxRouter.put(`${baseApi}/:id`, (req, res, next) => {
+    sandboxUpdateController.handleController(req, res, next);
+});
+
+sandboxRouter.get(`${baseApi}/:id`, (req, res, next) => {
+    sandboxGetByIdController.handleController(req, res, next);
 });
