e3a3d86e2f1ecc586d98621115917660b049a8ba
washington-shoji <shoji.washington@gmail.com>
2022-01-14 10:28:06 +1100

    refactored the sandbox router

--- src/routes/sandboxRoutes.ts
+++ src/routes/sandboxRoutes.ts
@@ -1,31 +1,29 @@
-import {
-    sandboxCreateController,
-    sandboxDeleteController,
-    sandboxUpdateController,
-    sandboxGetByIdController,
-    sandboxGetAllController,
-} from '../features/sandbox/controller/sandboxImpControllers';
 import express from 'express';
+import { sandboxCreateController } from '../features/sandbox/controller/sandbox_controller/sandboxCreateController';
+import { sandboxDeleteController } from '../features/sandbox/controller/sandbox_controller/sandboxDeleteController';
+import { sandboxGetAllController } from '../features/sandbox/controller/sandbox_controller/sandboxGetAllController';
+import { sandboxGetByIdController } from '../features/sandbox/controller/sandbox_controller/sandboxGetByIdController';
+import { sandboxUpdateController } from '../features/sandbox/controller/sandbox_controller/sandboxUpdateController';
 
 export const sandboxRouter = express.Router();
 
 const baseApi = '/sandbox';
 
-sandboxRouter.get(`${baseApi}`, (req, res, next) => {
+sandboxRouter.get(`${baseApi}/get`, (req, res, next) => {
     sandboxGetAllController.handleController(req, res, next);
 });
 
-sandboxRouter.post(`${baseApi}`, (req, res, next) => {
+sandboxRouter.post(`${baseApi}/create`, (req, res, next) => {
     sandboxCreateController.handleController(req, res, next);
 });
 
-sandboxRouter.delete(`${baseApi}/:id`, (req, res, next) => {
+sandboxRouter.delete(`${baseApi}/delete/:id`, (req, res, next) => {
     sandboxDeleteController.handleController(req, res, next);
 });
-sandboxRouter.put(`${baseApi}/:id`, (req, res, next) => {
+sandboxRouter.put(`${baseApi}/update/:id`, (req, res, next) => {
     sandboxUpdateController.handleController(req, res, next);
 });
 
-sandboxRouter.get(`${baseApi}/:id`, (req, res, next) => {
+sandboxRouter.get(`${baseApi}/get/:id`, (req, res, next) => {
     sandboxGetByIdController.handleController(req, res, next);
 });
