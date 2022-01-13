de3d63f80cf87736d2e9e6ce776121ad5c795691
washington-shoji <shoji.washington@gmail.com>
2022-01-13 14:00:20 +1100

    refactored the sandbox interface

--- src/features/sandbox/interface/sandbox.interface.ts
+++ src/features/sandbox/interface/sandbox.interface.ts
@@ -1,11 +1,18 @@
 import { Document } from 'mongoose';
+import { Request } from 'express';
 
 export interface ISandboxDocument extends Document {
     title: string;
     body: string;
 }
 
-// export interface ISandbox {
-//     title: string;
-//     body: string;
-// }
+export interface ISandboxService {
+    createSandboxEntry(
+        sandboxData: ISandboxDocument
+    ): Promise<ISandboxDocument>;
+    updateSandboxEntry(req: Request): Promise<ISandboxDocument>;
+
+    getSandboxEntries(): Promise<ISandboxDocument[]>;
+    getSandboxEntriesById(req: Request): Promise<ISandboxDocument>;
+    deleteSandboxEntry(req: Request): Promise<ISandboxDocument>;
+}
