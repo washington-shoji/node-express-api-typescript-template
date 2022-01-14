365ee063fcc270f30a1781e0acc2c92e41d13137
washington-shoji <shoji.washington@gmail.com>
2022-01-15 08:56:07 +1100

    added token to the utils, this will create and verify the token in the app life cycle

---
+++ src/utils/token/token.ts
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { IUserDocument } from './../../features/user/interface/user.interface';
+import { IToken } from './token.interface';
+export const createToken = (user: IUserDocument): string => {
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET as jwt.Secret, {
+        expiresIn: '1d',
+    });
+};
+
+export const verifyToken = async (
+    token: string
+): Promise<jwt.VerifyErrors | IToken> => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(
+            token,
+            process.env.JWT_SECRET as jwt.Secret,
+            (err, payload) => {
+                if (err) return reject(err);
+
+                resolve(payload as IToken);
+            }
+        );
+    });
+};
