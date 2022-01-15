1f7faf966fd0679b46791c483d384ece9168b1a7
washington-shoji <shoji.washington@gmail.com>
2022-01-15 11:30:15 +1100

    added user routes to the index file

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import validateEnv from './utils/validateEnv';
 import App from './app';
 import { sandboxRouter } from './routes/sandboxRoutes';
 import { rootRouter } from './routes/rootRouters';
+import { userRouter } from './routes/userRoutes';
 
 validateEnv();
 
-const app = new App(Number(process.env.PORT), [sandboxRouter, rootRouter]);
+const app = new App(Number(process.env.PORT), [
+    sandboxRouter,
+    rootRouter,
+    userRouter,
+]);
 
 app.listen();
