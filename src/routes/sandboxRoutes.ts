84c00634683fa16cf3da7098f3f2a73a2a6f167f
washington-shoji <shoji.washington@gmail.com>
2022-01-15 13:25:54 +1100

    minor refactoring

--- src/routes/sandboxRoutes.ts
+++ src/routes/sandboxRoutes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import authenticatedMiddleware from '../middleware/autheticate/authenticate.middleware';
 import { sandboxCreateController } from '../features/sandbox/controller/sandbox_controller/sandboxCreateController';
 import { sandboxDeleteController } from '../features/sandbox/controller/sandbox_controller/sandboxDeleteController';
 import { sandboxGetAllController } from '../features/sandbox/controller/sandbox_controller/sandboxGetAllController';
@@ -9,21 +10,41 @@ export const sandboxRouter = express.Router();
 
 const baseApi = '/sandbox';
 
-sandboxRouter.get(`${baseApi}/get`, (req, res, next) => {
-    sandboxGetAllController.handleController(req, res, next);
-});
+sandboxRouter.get(
+    `${baseApi}/get`,
+    authenticatedMiddleware,
+    (req, res, next) => {
+        sandboxGetAllController.handleController(req, res, next);
+    }
+);
 
-sandboxRouter.post(`${baseApi}/create`, (req, res, next) => {
-    sandboxCreateController.handleController(req, res, next);
-});
+sandboxRouter.post(
+    `${baseApi}/create`,
+    authenticatedMiddleware,
+    (req, res, next) => {
+        sandboxCreateController.handleController(req, res, next);
+    }
+);
 
-sandboxRouter.delete(`${baseApi}/delete/:id`, (req, res, next) => {
-    sandboxDeleteController.handleController(req, res, next);
-});
-sandboxRouter.put(`${baseApi}/update/:id`, (req, res, next) => {
-    sandboxUpdateController.handleController(req, res, next);
-});
+sandboxRouter.delete(
+    `${baseApi}/delete/:id`,
+    authenticatedMiddleware,
+    (req, res, next) => {
+        sandboxDeleteController.handleController(req, res, next);
+    }
+);
+sandboxRouter.put(
+    `${baseApi}/update/:id`,
+    authenticatedMiddleware,
+    (req, res, next) => {
+        sandboxUpdateController.handleController(req, res, next);
+    }
+);
 
-sandboxRouter.get(`${baseApi}/get/:id`, (req, res, next) => {
-    sandboxGetByIdController.handleController(req, res, next);
-});
+sandboxRouter.get(
+    `${baseApi}/get/:id`,
+    authenticatedMiddleware,
+    (req, res, next) => {
+        sandboxGetByIdController.handleController(req, res, next);
+    }
+);
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import authenticatedMiddleware from '../middleware/autheticate/authenticate.middleware';
+import { userGetController } from '../features/user/controllers/userGet.controller';
+import { userLoginController } from '../features/user/controllers/userLogin.controller';
 import { userRegisterController } from '../features/user/controllers/userRegister.controller';
 
 export const userRouter = express.Router();
@@ -8,3 +11,11 @@ const baseApi = '/user';
 userRouter.post(`${baseApi}/register`, (req, res, next) => {
     userRegisterController.handleController(req, res, next);
 });
+
+userRouter.post(`${baseApi}/login`, (req, res, next) => {
+    userLoginController.handleController(req, res, next);
+});
+
+userRouter.get(`${baseApi}/get`, authenticatedMiddleware, (req, res, next) =>
+    userGetController.getUser(req, res, next)
+);
