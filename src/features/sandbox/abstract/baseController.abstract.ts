a30ec446c1126b535621f66eabd407763378c650
washington-shoji <shoji.washington@gmail.com>
2022-01-17 16:53:35 +1100

    refactored the sandbox base controller abstracted class

--- src/features/sandbox/abstract/baseController.abstract.ts
+++ src/features/sandbox/abstract/baseController.abstract.ts
@@ -1,30 +1,80 @@
 import { Request, Response, NextFunction } from 'express';
 
-export abstract class BaseController<T, U, W> {
-    private readonly service: T;
+export abstract class NewBaseController {
+    abstract controller(req: Request, res: Response, next: NextFunction): void;
 
-    constructor(service: T) {
-        this.service = service;
+    protected constructor() {}
+
+    protected Error(error: any, next: NextFunction): void {
+        throw new Error();
+    }
+}
+
+export abstract class BaseController<T, U, W> extends NewBaseController {
+    protected request: Request | undefined;
+
+    protected constructor() {
+        super();
     }
 
-    protected abstract successCode(): number;
-    protected abstract successMessage(): string;
-    protected abstract parseQueryParams(parseArgs: Request): U;
+    protected abstract authoriseUser(): void;
 
-    protected abstract processData(parsedData: U): Promise<W>;
+    protected abstract successMessage(): string;
+
+    protected parseUrlParams(): U {
+        // @ts-ignore
+        return this.request.params;
+    }
+    protected parseQueryParams(request: Request): W {
+        // @ts-ignore
+        return this.request.query;
+    }
+
+    protected abstract retrieveData(): Promise<T>;
     protected Error(error: any, next: NextFunction): void {
         throw new Error();
     }
 
-    public async handleController(
+    public async controller(
         req: Request,
         res: Response,
         next: NextFunction
     ): Promise<void> {
         try {
-            const result = await this.processData(this.parseQueryParams(req));
-            res.status(this.successCode()).json({
+            this.request = req;
+            await this.authoriseUser();
+            const result: T = await this.retrieveData();
+            this.request = undefined;
+            res.status(200).json({
                 message: this.successMessage(),
+                result,
+            });
+        } catch (error: any) {
+            next(this.Error(error, next));
+        }
+    }
+}
+
+export abstract class NewBasePostController<T> extends NewBaseController {
+    protected request: Request | undefined;
+    protected constructor() {
+        super();
+    }
+
+    abstract parseRequestBody(): T;
+    abstract processData(): Promise<T>;
+
+    public async controller(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            this.request = req;
+            const result: T = await this.processData();
+            this.request = undefined;
+            res.status(201).json({
+                message: 'aaa',
                 result,
             });
         } catch (error: any) {
